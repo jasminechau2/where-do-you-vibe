b51539bd2b203145d3b1b6a13f4746dd3e49cf0f
Jasmine Chau <jchau@middlebury.edu>
2021-03-17 12:55:50 -0400

    first deployment

---
+++ src/map.js
@@ -0,0 +1,33 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+function Map() {
+    return(
+        <div>
+            <header>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+            integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+            crossorigin=""/>
+            <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+            integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+            crossorigin=""></script>
+            </header>
+            <main>
+            <div id="mapid">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                 <Marker position={[51.505, -0.09]}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+            </MapContainer>
+            </div>
+            </main>
+        </div>
+    );
+
+}
+export default Map;
