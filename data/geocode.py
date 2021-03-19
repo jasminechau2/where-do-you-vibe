84d49420e3cfe248301e7ff1592c2f5c35ab85e6
zeinhorn@middlebury.edu <>
2021-03-19 13:24:52 -0400

    add all coordinates to csv

--- data/geocode.py
+++ data/geocode.py
@@ -5,15 +5,14 @@ import csv
 key = "a9fc6969619b4da09337d152b416c1f2"  # get api key from:  https://opencagedata.com
 geocoder = OpenCageGeocode(key)
 
-with open('lat-lng1.csv', 'w', newline='') as write_file:
+with open('lat-lng2.csv', 'w', newline='') as write_file:
     writer = csv.writer(write_file)
     writer.writerow(["City", "Country", "lat", "lng"])
     with open('places.csv') as read_file:
         csv_reader = csv.reader(read_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+            if (line_count <= 1829):
                 line_count += 1
             else:
                 query = row[0] +", "+ row[1]
@@ -21,9 +20,9 @@ with open('lat-lng1.csv', 'w', newline='') as write_file:
                 results = geocoder.geocode(query)
                 lat = results[0]['geometry']['lat']
                 lng = results[0]['geometry']['lng']
-                print (lat, lng)
+                print (lat, lng, row[0])
                 writer.writerow([row[0], row[1], lat, lng])
 
                 line_count += 1
-                if(line_count == 1830):
+                if(line_count == 3662):
                     break
