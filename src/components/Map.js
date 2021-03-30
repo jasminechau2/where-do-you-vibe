b083886b6751f80c6e54205c51d7a67cd90e20d6
Jasmine Chau <jchau@middlebury.edu>
2021-03-30 15:02:21 -0400

    added the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,43 +1,60 @@
-import React, { Component } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React from "react"
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-function App() {
-    const position = [51.505, -0.09]
-  
-    return (
-      <div>
-        <header>
-          <p>
-            hello Jasmine 
-          </p>
-          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-         integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-         crossorigin=""/>
-         <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-         integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-         crossorigin=""></script>
-        </header>
-        <body>
-            <MapContainer 
-         center={[51.505, -0.09]}
-         zoom={13}
-         scrollWheelZoom={false}
-         style={{height: '100vh'}}> 
+
+function Map() {
+  // setup
+  const provider = new OpenStreetMapProvider();
+  const position = [0,0]
+
+  return (
+    <div >
+       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+       integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+       crossorigin=""/>
+        <MapContainer 
+            center={[0,0]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{height: '100vh'}}> 
               <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={position}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </MapContainer>
-          
-        </body>
-      </div>
-      
-    );
-  }
-  
-  export default App;
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+
+                <Marker position={position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+          </MapContainer>
+    </div>
+    
+  );
+}
+
+export default Map;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
+import Map from './components/Map.js'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
+    <Map/>
   </React.StrictMode>,
   document.getElementById('root')
 );
