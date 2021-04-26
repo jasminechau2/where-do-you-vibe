b5ed49d368f5f3eb5d2236fa95991679a041f29b
Jasmine Chau <jchau@middlebury.edu>
2021-04-26 14:17:16 -0400

    pop ups on map move when top cities are generated

--- client/src/components/user-cities-display-template.js
+++ client/src/components/user-cities-display-template.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 /*
 * This component lists the user's top cities that match with their top genres 
 */
 export default function UserCityDisplayTemplate({citiesObject, callback}) {
-    console.log(citiesObject);
     const cities = citiesObject[0];
     const countries = citiesObject[1]; 
     const combine = [];
@@ -15,13 +13,13 @@ export default function UserCityDisplayTemplate({citiesObject, callback}) {
                  key = {cities[i]}>
                  {cities[i]}, {countries[i]}
                  </li>
-             );
+             )
            };
-    }   
-   
-    //callback(combine);
+    };
+
+    
+
     return ( 
-   
     <div>
         {combine}
     </div>
